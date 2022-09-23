a79a973afb0c94e41d807c9b4d3070f658239b79
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2022-09-22 22:19:17 -0300

    22-09

--- ViewModels/Details.cs
+++ ViewModels/Details.cs
@@ -4,10 +4,10 @@ namespace Pokedex.ViewModels
 {
     public class Details
     {
-        public Pokemons? Prior {get; set; }
+        public Pokemons Prior {get; set; }
 
-        public Pokemons? Current {get; set; }
+        public Pokemons Current {get; set; }
 
-        public Pokemons? Next {get; set; }
+        public Pokemons Next {get; set; }
     }
 }
